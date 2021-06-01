7c3af952a9b434b2584cf1f42bf8d3e879bb9eda
saikrishna <saikrishna31120@gmail.com>
2021-06-01 10:27:19 +0530

    fixed tensorflow memory leak
    Co-authored-by:
    Nikila B
    <nikila22babu@gmail.com>

--- js/people.js
+++ js/people.js
@@ -6,6 +6,8 @@ class Box
         if (typeof(size) === typeof(1))
             size = [size,size];
         this.size = size;
+        if (typeof(pos) === typeof(1))
+            pos = [pos,pos];
         this.pos = pos;
         this.num = 0;
         this.inf_c = 0;
@@ -22,25 +24,27 @@ class Box
 
     add_people(num,inf)
     {
-        this.p_x = tf.concat([this.p_x,tf.randomUniform([num], this.pos[0], this.pos[0]+this.size[0],'float32')]);
-        this.p_y = tf.concat([this.p_y,tf.randomUniform([num], this.pos[1],this.pos[1]+this.size[1],'float32')]);
-        this.p_state = tf.concat([tf.tensor(this.p_state),tf.multinomial(tf.tensor([.9,.1]),num)]).arraySync();
+        this.p_x = tf.tidy(()=>tf.concat([this.p_x,tf.randomUniform([num], this.pos[0], this.pos[0]+this.size[0],'float32')]));
+        this.p_y = tf.tidy(()=>tf.concat([this.p_y,tf.randomUniform([num], this.pos[1],this.pos[1]+this.size[1],'float32')]));
+        this.p_state = tf.tidy(()=>tf.concat([tf.tensor(this.p_state),tf.multinomial(tf.tensor([.9,.1]),num)]).arraySync());
         this.num+=num;
     }
     get_info()
     {
       
-        return tf.stack([this.p_x,this.p_y,tf.tensor(this.p_state)]).transpose().arraySync();
+        return tf.tidy(()=>tf.stack([this.p_x,this.p_y,tf.tensor(this.p_state)]).transpose().arraySync());
     }
     get_box_info(){
         return [this.pos,this.size];
     }
-    
+    mod(n, m) {
+        return ((n % m) + m) % m;
+    }
     bounce_x(ar){
         var arb = [];
         for(var i = 0; i<this.num;i++)
         {
-            arb.push(this.pos[0]+this.size[0] - Math.abs(this.size[0]- ((ar[i]-this.pos[0]) % (2*this.size[0]))));
+            arb.push(this.pos[0]+this.size[0] - Math.abs(this.size[0]- this.mod(ar[i]-this.pos[0] , 2*this.size[0])));
         }
         return tf.tensor(arb)
     }
@@ -48,59 +52,62 @@ class Box
     bounce_y(ar){
         var arb = [];
         for(var i = 0; i<this.num;i++){
-            arb.push(this.pos[1]+this.size[1] - Math.abs(this.size[1]- ((ar[i]-this.pos[1]) % (2*this.size[1]))));
+            arb.push(this.pos[1]+this.size[1] - Math.abs(this.size[1]- this.mod(ar[i]-this.pos[1] , 2*this.size[1])));
         }
         return tf.tensor(arb);
     }
 
     move_people(displacement)
     {
-        var sum_x = tf.add(this.p_x,displacement[0]);
-        var sum_y = tf.add(this.p_y,displacement[1]);
-        this.p_x = this.bounce_x((sum_x).arraySync());
-        this.p_y = this.bounce_x((sum_y).arraySync());                     
+        var sum_x = tf.tidy(()=>tf.add(this.p_x,displacement[0]));
+        var sum_y = tf.tidy(()=>tf.add(this.p_y,displacement[1]));
+        this.p_x.dispose()
+        this.p_y.dispose()
+        this.p_x = tf.tidy(()=>this.bounce_x((sum_x).arraySync()));
+        this.p_y = tf.tidy(()=>this.bounce_x((sum_y).arraySync()));  
+        sum_x.dispose()
+        sum_y.dispose()             
         this.now = this.now + 0.1;
     }
 
     update_state(r,t,p_r,p_t)
     {
-        var zeros = tf.zeros([this.num])
-        var ones = tf.fill([this.num],1)
-        var dummy = tf.fill([this.num],-100)
-        var index = tf.range(0,this.num,1)
-        var state_t = tf.tensor(this.p_state)
-        state_t.print()
-        var sus_x =  tf.where(tf.equal(state_t,zeros),this.p_x,dummy);
-        var sus_y =  tf.where(tf.equal(state_t,zeros),this.p_y,dummy);
-        var inf =  tf.where(tf.equal(state_t,ones),index,dummy);
-        var inf_cur = []
-        for(let i =0; i<inf.size;i++)
-        {
-            
-            var id = tf.slice(inf,i,1).dataSync()[0]
-            if(id!=-100)
+        var inf_cur = tf.tidy(()=>{
+            var zeros = tf.zeros([this.num])
+            var ones = tf.fill([this.num],1)
+            var dummy = tf.fill([this.num],-100)
+            var index = tf.range(0,this.num,1)
+            var state_t = tf.tensor(this.p_state)
+            var sus_x =  tf.where(tf.equal(state_t,zeros),this.p_x,dummy);
+            var sus_y =  tf.where(tf.equal(state_t,zeros),this.p_y,dummy);
+            var inf =  tf.where(tf.equal(state_t,ones),index,dummy);
+            var inf_cur = []
+            for(let i =0; i<inf.size;i++)
             {
-                var x_inf = tf.fill([this.num],tf.slice(this.p_x,id,1).dataSync());
-                var y_inf = tf.fill([this.num],tf.slice(this.p_y,id,1).dataSync()); 
-                var result = tf.where(tf.greater(tf.sub(r*r,tf.add(tf.squaredDifference(sus_x,x_inf),tf.squaredDifference(sus_y,y_inf))),tf.zeros([this.num])),index,dummy)
-                inf_cur = inf_cur.concat(result.arraySync())
-                if(this.now-this.p_inf_time[id] >= t)
+                var id = tf.slice(inf,i,1).dataSync()[0]
+                if(id!=-100)
                 {
-                    this.p_state[id]=1+tf.multinomial(tf.tensor([1-p_t,p_t]),1).arraySync()[0]
+                    var x_inf = tf.fill([this.num],tf.slice(this.p_x,id,1).dataSync());
+                    var y_inf = tf.fill([this.num],tf.slice(this.p_y,id,1).dataSync()); 
+                    var result = tf.where(tf.greater(tf.sub(r*r,tf.add(tf.squaredDifference(sus_x,x_inf),tf.squaredDifference(sus_y,y_inf))),tf.zeros([this.num])),index,dummy)
+                    inf_cur = inf_cur.concat(result.arraySync())
+                    if(this.now-this.p_inf_time[id] >= t)
+                    {
+                        this.p_state[id]=1+tf.multinomial(tf.tensor([1-p_t,p_t]),1).arraySync()[0]
+                    }
                 }
             }
-        }
-        inf_cur = [].concat(...inf_cur)
-        console.log(inf_cur)
-        inf_cur = [...new Set(inf_cur.filter(item => item != -100))]
-        console.log(inf_cur)
+            inf_cur = [].concat(...inf_cur)
+            inf_cur = [...new Set(inf_cur.filter(item => item != -100))]
+            return inf_cur
+        })
+        
         var x;
         for(x in inf_cur)
         {
-            this.p_state[inf_cur[x]]=tf.multinomial(tf.tensor([1-p_r,p_r]),1).arraySync()[0] //probability
+            this.p_state[inf_cur[x]]=tf.tidy(()=>tf.multinomial(tf.tensor([1-p_r,p_r]),1).arraySync()[0]) //probability
             this.p_inf_time[inf_cur[x]]=this.now
         }
-        console.log(this.p_state)
     }
 
     set_dest(max)
@@ -112,30 +119,32 @@ class Box
         var y 
         for (i in [...Array(this.num).keys()])
         {
-            x = tf.randomNormal([1],this.p_x.arraySync()[i],tf.randomUniform([1],0,max).arraySync()[0]).arraySync()[0];
-            y = tf.randomNormal([1],this.p_y.arraySync()[i],tf.randomUniform([1],0,max).arraySync()[0]).arraySync()[0];
+            x = tf.tidy(()=>tf.randomNormal([1],this.p_x.arraySync()[i],tf.randomUniform([1],0,max).arraySync()[0]).arraySync()[0]);
+            y = tf.tidy(()=>tf.randomNormal([1],this.p_y.arraySync()[i],tf.randomUniform([1],0,max).arraySync()[0]).arraySync()[0]);
             dest_x.push(x);
             dest_y.push(y);
         }
-        this.p_dest_x = tf.tensor(dest_x);
-        this.p_dest_y = tf.tensor(dest_y);
+        this.p_dest_x.dispose()
+        this.p_dest_y.dispose()
+        this.p_dest_x = tf.tidy(()=>tf.tensor(dest_x));
+        this.p_dest_y = tf.tidy(()=>tf.tensor(dest_y));
         this.set_dest_bounds()
     }
 
     set_dest_bounds()
     {
-        this.p_dest_x = this.bounce_x(this.p_dest_x.arraySync())
-        this.p_dest_y = this.bounce_y(this.p_dest_y.arraySync())
+        this.p_dest_x = tf.tidy(()=>this.bounce_x(this.p_dest_x.arraySync()));
+        this.p_dest_y = tf.tidy(()=>this.bounce_y(this.p_dest_y.arraySync()));
     }
 
     move_to_dest(speed)
     {
-        var s
-        var disp_x
-        var disp_y
-        s = tf.scalar(speed);
-        disp_x = tf.mul(s,tf.sub(this.p_dest_x,this.p_x));
-        disp_y = tf.mul(s,tf.sub(this.p_dest_y,this.p_y));
+        var s = tf.scalar(speed);
+        var disp_x = tf.tidy(()=>tf.mul(s,tf.sub(this.p_dest_x,this.p_x)));
+        var disp_y = tf.tidy(()=>tf.mul(s,tf.sub(this.p_dest_y,this.p_y)));
         this.move_people([disp_x,disp_y]);
+        disp_x.dispose()
+        disp_y.dispose()
+        s.dispose()
     }
 }
