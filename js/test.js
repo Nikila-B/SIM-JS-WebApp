46e4ad272534aee3c283e4f22123db518e7d2361
Nikila-B <nikila22babu@gmail.com>
2021-07-01 23:37:39 +0530

    Added 3 lines along with axis labelling

--- js/test.js
+++ js/test.js
@@ -79,7 +79,6 @@ circle = svg.selectAll('circles')
         return colors[d[2]]; // new data field
     });
 var dest=[[200,400,1],[100,50,0]]
-
 circle.data(dest)
     .join('circle')
     .transition()
@@ -109,115 +108,14 @@ circle.data(dest)
         
     }) */
 
-const create_btn = document.getElementById('create')    
-const start_btn = document.getElementById("start")
-const add_btn = document.getElementById("add_btn")
-const move_btn = document.getElementById("move_btn")
-const remove_btn = document.getElementById("remove_btn")
-const action_grp = document.getElementsByName("action");
-const action_grp_btns = document.getElementsByName("action_grp");
-        
-if(create_btn.checked==true){
-    start_btn.disabled=true
-    action_grp_btns.forEach((el)=>{
-        el.className='switch'
-    })
-}
-else{
-    start_btn.disabled=false
-    action_grp.forEach((el)=>{
-        el.checked=false
-    });
-    action_grp_btns.forEach((el)=>{
-        el.className='switch disabled'
-    })
-}
-start_btn.innerHTML='start'
-
-function selectOnlyThis(id){
-    action_grp.forEach((el)=>{
-        if(el!=id)
-            el.checked = false;
-    });
-}
-  
-  
-
-
-
-const svg = create_svg(500,500);
-let obj = new Box("sim1",0,500);
-obj.add_people(10,0.4);
-obj.add_people(50,0.2)
-var info = obj.get_info();
-const ar1 = obj.get_box_info();
-//const ar2 = obj[ar,ar1]
-circle = init_circles(svg,info);
-obj.set_dest(200);
-//console.log(tf.memory().numTensors)
-info_buffer=[]
-info_buffer.push(info)
-var n = 500
-var data2 = [
-    {time: 0, inf: 0},
- ];
- var data1 = [
-    {time: 0, inf: 0},
- ];
-
-console.log(data2)
-for(let i =1;i<n;i++){
-    info_buffer.push(obj.get_info());
-    data2.push({time:obj.now,inf:obj.count_state(1)})
-    obj.move_to_dest(0.01);
-    obj.update_state(25,20,0.5,0.5)
-    //console.log(obj.update_dest(1))
-    if (obj.update_dest(1) == true)
-    {
-        obj.set_dest(200);
-    }
-}
-
-let animateTimer;
-let isAnimating = false
-let completed = false
-let i=0;
-start_btn.addEventListener("click", function(){
-        if(completed==true){
-            i=0
-            start_btn.innerHTML="Start";
-            completed=false
-            move_circles(circle,info_buffer[0],0,0,0)
+    const create_btn = document.getElementById('create')    
+    const start_btn = document.getElementById("start")
+    const add_btn = document.getElementById("add_btn")
+    const move_btn = document.getElementById("move_btn")
+    const remove_btn = document.getElementById("remove_btn")
+    const action_grp = document.getElementsByName("action");
+    const action_grp_btns = document.getElementsByName("action_grp");
             
-        }
-        else if (isAnimating) {
-            clearInterval(animateTimer)    
-            start_btn.innerHTML="Resume";
-            isAnimating=false                                        
-        }
-        else {
-            isAnimating=true
-            start_btn.innerHTML="Pause";
-            document.getElementById('create-btn').className='switch disabled'
-            animateTimer = setInterval(function(){
-                if(i<n-1){
-                        i++;
-                        move_circles(circle,info_buffer[i],30,i,25);
-                        updateData(svg1,data2.slice(0,i),x,y,xAxis,yAxis,valueLine,data2[i].time);
-                }
-                else{
-                    clearInterval(animateTimer)
-                    start_btn.innerHTML="Reset";
-                    document.getElementById('create-btn').className='switch'
-                    isAnimating=false
-                    completed=true
-                }
-            },30)
-        }
-        
-    
-});
-create_btn.addEventListener('click',function(){
     if(create_btn.checked==true){
         start_btn.disabled=true
         action_grp_btns.forEach((el)=>{
@@ -233,70 +131,161 @@ create_btn.addEventListener('click',function(){
             el.className='switch disabled'
         })
     }
-});
-
-
-create_axis()
-x = get_x()
-y = get_y()
-xAxis = get_xaxis(x)
-yAxis = get_yaxis(y)
-valueLine = get_line(x,y)
-svg1 = create_trend_svg()
-add_data(svg1,data1,x,y,xAxis,yAxis,valueLine,obj.num)
-
-
-/*
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 50, left: 70},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-
-// set the ranges
-var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-// define the line
-var valueline = d3.line()
-    .x(function(d) { return x(d.time); })
-    .y(function(d) { return y(d.inf); });
-
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// Get the data
-function plot(data) {
-
-
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.time; }));
-  y.domain([0, d3.max(data, function(d) { return d.inf; })]);
-
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", valueline)
-      .attr('fill','none')
-      .attr('stroke','black')
-
-  // Add the x Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  // Add the y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
-
-}
-plot(data2)
-*/
+    start_btn.innerHTML='start'
+    
+    function selectOnlyThis(id){
+        action_grp.forEach((el)=>{
+            if(el!=id)
+                el.checked = false;
+        });
+    }
+      
+      
+    
+    
+    
+    const svg = create_svg(500,500);
+    let obj = new Box("sim1",0,500);
+    obj.add_people(10,0.4);
+    obj.add_people(50,0.2)
+    var info = obj.get_info();
+    const ar1 = obj.get_box_info();
+    //const ar2 = obj[ar,ar1]
+    circle = init_circles(svg,info);
+    obj.set_dest(200);
+    //console.log(tf.memory().numTensors)
+    info_buffer=[]
+    info_buffer.push(info)
+    var n = 500
+    
+    var data = [{time: 0, inf: 0}]
+    var data1 = [{time: 0,inf: 0}]
+    var data2 = [{time: 0,inf: 0}]
+    var data3 = [{time: 0,inf: 0}]
+    for(let i =1;i<n;i++){
+        info_buffer.push(obj.get_info());
+        data1.push({time:obj.now,inf:obj.count_state(1)})
+        data2.push({time:obj.now,inf:obj.count_state(0)})
+        data3.push({time:obj.now,inf:obj.count_state(2)})
+        obj.move_to_dest(0.01);
+        obj.update_state(25,20,0.5,0.5)
+        if (obj.update_dest(1) == true)
+        {
+            obj.set_dest(200);
+        }
+    }
+    
+    let animateTimer;
+    let isAnimating = false
+    let completed = false
+    let i=0;
+    start_btn.addEventListener("click", function(){
+            if(completed==true){
+                i=0
+                start_btn.innerHTML="Start";
+                completed=false
+                move_circles(circle,info_buffer[0],0,0,0)
+                
+            }
+            else if (isAnimating) {
+                clearInterval(animateTimer)    
+                start_btn.innerHTML="Resume";
+                isAnimating=false                                        
+            }
+            else {
+                isAnimating=true
+                start_btn.innerHTML="Pause";
+                document.getElementById('create-btn').className='switch disabled'
+                animateTimer = setInterval(function(){
+                    if(i<n-1)
+                    {
+                            i++;
+                            move_circles(circle,info_buffer[i],30,i,25);
+                            updateData(svg1,data1.slice(0,i),data2.slice(0,i),data3.slice(0,i),x,y,xAxis,yAxis,valueline1,valueline2,data2[i].time);
+                    }
+                    else{
+                        clearInterval(animateTimer)
+                        start_btn.innerHTML="Reset";
+                        document.getElementById('create-btn').className='switch'
+                        isAnimating=false
+                        completed=true
+                    }
+                },30)
+            }
+            
+        
+    });
+    create_btn.addEventListener('click',function(){
+        if(create_btn.checked==true){
+            start_btn.disabled=true
+            action_grp_btns.forEach((el)=>{
+                el.className='switch'
+            })
+        }
+        else{
+            start_btn.disabled=false
+            action_grp.forEach((el)=>{
+                el.checked=false
+            });
+            action_grp_btns.forEach((el)=>{
+                el.className='switch disabled'
+            })
+        }
+    });
+    
+    
+    create_axis()
+    x = get_x()
+    y = get_y()
+    xAxis = get_xaxis(x)
+    yAxis = get_yaxis(y)
+    valueline1 = get_line_inf(x,y)
+    valueline2 = get_line_sus(x,y)
+    valueline3 = get_line_rec(x,y)
+    svg1 = create_trend_svg()
+    add_data(svg1,data,x,y,xAxis,yAxis,valueline1,valueline2,valueline3,obj.num)
+    
+    
+    /*
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 50, left: 70},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+    // set the ranges
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+    // define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d.time); })
+        .y(function(d) { return y(d.inf); });
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+    // Get the data
+    function plot(data) {
+      // Scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d.time; }));
+      y.domain([0, d3.max(data, function(d) { return d.inf; })]);
+      // Add the valueline path.
+      svg.append("path")
+          .data([data])
+          .attr("class", "line")
+          .attr("d", valueline)
+          .attr('fill','none')
+          .attr('stroke','black')
+      // Add the x Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+      // Add the y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y));
+    }
+    plot(data2)
+    */
